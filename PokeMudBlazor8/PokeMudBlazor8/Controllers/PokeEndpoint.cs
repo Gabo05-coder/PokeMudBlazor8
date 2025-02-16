f0e3769272c08a2934b54c916fd0eb7cdf0926dd
Gabriel Quintana <82337170+Gabo05-coder@users.noreply.github.com>
2025-02-15 19:27:41 -0500

    Creando Controlador

---
+++ PokeMudBlazor8/PokeMudBlazor8/Controllers/PokeEndpoint.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using PokeMudBlazor8.Components.Services;
+
+namespace PokeMudBlazor8.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly PokemonService _pokemonService;
+
+        public PokemonController(PokemonService pokemonService)
+        {
+            _pokemonService = pokemonService;
+        }
+
+        // Endpoint para obtener una lista de pokemons
+        [HttpGet("pokemons")]
+        public async Task<IActionResult> GetPokemons(int count = 4)
+        {
+            try
+            {
+                var pokemons = await _pokemonService.GetPokemonsAsync(count);
+                return Ok(pokemons);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
